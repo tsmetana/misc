03d0c22f8e44f3ddbe639bb496d1f14405436ec4
Tomas Smetana <tsmetana@redhat.com>
2017-04-18 11:58:12 +0200

    use events instead of conditions to stop

--- aws_test_script/test_adc.py
+++ aws_test_script/test_adc.py
@@ -3,13 +3,13 @@
 import sys
 import threading
 import time
-import supbprocess
+import subprocess
 import glob
 import os
 import signal
 import random
 
-exit_flag = False
+exit_flag = threading.Event()
 pods_limit = 20
 pvc_limit = 5
 
@@ -28,8 +28,7 @@ spec:
     - ReadWriteOnce
   resources:
     requests:
-      storage: 500Mi
-"""
+      storage: 500Mi"""
 
     pod_template = """
 apiVersion: v1
@@ -56,23 +55,26 @@ spec:
   volumes:
       - name: vol
         persistentVolumeClaim:
-          claimName: testclaim%(claim_num)s
-"""
-    def __init__:
+          claimName: testclaim%(claim_num)s"""
+
+    def __init__(self):
         self.pods = set()
         self.pvcs = set()
 
     def _kubectl_create(self, file_arg):
-        subprocess.run("cluster/kubectl.sh create -f %s" % file_arg, , shell=True, check=True)
+        try:
+            subprocess.run("cluster/kubectl.sh create -f %s" % file_arg, shell=True, check=True)
+        except:
+            print("Error creating resource from %s" % file_arg)
 
     def create_pvc_yaml(self, claim_num):
-        filename = "/tmp/pvc-" + claim_num + ".yaml"
+        filename = "/tmp/pvc-" + str(claim_num) + ".yaml"
         with open(filename, 'w') as f:
             f.write(self.pvc_template % { 'claim_num': claim_num })
         return filename
 
     def create_pod_yaml(self, pod_num, claim_num):
-        filename = "/tmp/pod-" + pod_num + ".yaml"
+        filename = "/tmp/pod-" + str(pod_num) + ".yaml"
         with open(filename, 'w') as f:
             f.write(self.pod_template % { 'pod_num': pod_num, 'claim_num': claim_num })
         return filename
@@ -80,7 +82,7 @@ spec:
     def create_pvc(self):
         if len(self.pvcs) < pvc_limit:
             pvc_num = 0
-            while pvc_num in self.pvcs
+            while pvc_num in self.pvcs:
                 pvc_num = round(random.random() * (pvc_limit - 1))
             yaml_file = self.create_pvc_yaml(pvc_num)
             if yaml_file != None:
@@ -89,30 +91,33 @@ spec:
                 #os.unlink(yaml_file)
 
     def create_pod(self):
-        if len(self.pods) < pod_limit:
+        if len(self.pods) < pods_limit:
             pod_num = 0
+            if len(self.pvcs) == 0:
+                return
             while pod_num in self.pods:
-                pod_num = round(random.random() * (pod_limit - 1))
-            claim_num = self.pvcs(round(random.random() * (len(self.pvcs) - 1)))
+                pod_num = round(random.random() * (pods_limit - 1))
+            claim_num = random.sample(self.pvcs, 1)[0]
             yaml_file = self.create_pod_yaml(pod_num, claim_num)
             if yaml_file != None:
-                self._kubectl_create(self_.yaml_file)
-                self.pods.add(pvc_num)
+                self._kubectl_create(yaml_file)
+                self.pods.add(claim_num)
                 #os.unlink(yaml_file)
 
     def delete_pod(self):
-        pod_num = self.pods(round(random.random() * (len(self.pods) - 1)))
-        subprocess.run("cluster/kubectl.sh delete pod busybox-test-%s --grace-period=1" % pod_num, shell=True)
+        if len(self.pods) > 0:
+            pod_num = random.sample(self.pods, 1)[0]
+            subprocess.run("cluster/kubectl.sh delete pod busybox-test-%s --grace-period=1" % pod_num, shell=True)
 
 class ControllerSpawner(threading.Thread):
-    controller_cmd = "sudo -E _output/local/bin/linux/amd64//hyperkube controller-manager --v=3 \
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.controller_process = None
+        self.controller_cmd = "sudo -E _output/local/bin/linux/amd64//hyperkube controller-manager --v=3 \
     --service-account-private-key-file=/tmp/kube-serviceaccount.key --root-ca-file=/var/run/kubernetes/server-ca.crt \
     --enable-hostpath-provisioner=false --pvclaimbinder-sync-period=15s --feature-gates=AllAlpha=true \
     --cloud-provider=aws --cloud-config= --kubeconfig /var/run/kubernetes/controller.kubeconfig \
     --use-service-account-credentials --master=https://localhost:6443"
-    def __init__(self):
-        threading.Thread.__init__(self)
-        self.controller_process = None
 
     def _find_pid(self):
         pid = None
@@ -135,30 +140,30 @@ class ControllerSpawner(threading.Thread):
             pid = self.controller_process.pid
         print("Killing the controller process PID %s" % pid)
         subprocess.run("sudo kill -KILL %s" % pid, shell=True)
-        self.controller_process.wait()
+        if self.controller_process != None:
+            self.controller_process.wait()
         print("Starting new controller process")
         try:
-            self.controller_process = subprocess.Popen(controller_cmd, shell=True)
+            self.controller_process = subprocess.Popen(self.controller_cmd, shell=True)
         except Exception as e:
             print ("Error starting new controller: {0}".format(e))
 
     def run(self):
         print("Starting ControllerSpawner")
-        wait_time = random.random) * 30
         # Wait for random time (0 - 30s) and force-restart the controller
-        while not threading.wait_for(exit_flag, timeout=wait_time):
+        while not exit_flag.is_set():
             self._restart_controller()
-            wait_time = random.random() * 30
+            wait_time = 20 + random.random() * 120
+            exit_flag.wait(timeout=wait_time)
 
 class PodSpawner(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.aws = ClusterObjects()
 
-    def run():
+    def run(self):
         print("Starting PodSpawner")
-        wait = random.random() * 2
-        while not threading.wait_for(exit_flag, timeout=wait):
+        while not exit_flag.is_set():
             case = round(random.random() * 2)
             if case == 0:
                 self.aws.create_pod()
@@ -166,17 +171,18 @@ class PodSpawner(threading.Thread):
                 self.aws.create_pvc()
             elif case == 2:
                 self.aws.delete_pod()
-
             wait = random.random() * 10
+            exit_flag.wait(timeout=wait)
 
 def main():
     ctrl_spawner = ControllerSpawner()
     pod_spawner = PodSpawner()
 
-    ctrl_spawner.start()
+    exit_flag.clear()
     pod_spawner.start()
-    time.sleep(180)
-    exit_flag = True
+    ctrl_spawner.start()
+    time.sleep(600) # 10 minutes
+    exit_flag.set()
     pod_spawner.join()
     ctrl_spawner.join()
     return 0
