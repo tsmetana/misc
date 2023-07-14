bf6e4acb2ed1065f68bb6dad23d6856eb869c901
Tomas Smetana <tomas@smetana.name>
2023-07-14 13:36:15 +0200

    local-kube: fixes for third party etcd and systemd-resolvd

--- local-kube.sh
+++ local-kube.sh
@@ -11,15 +11,19 @@ echo "Cleaning up /tmp"
 pushd /tmp
 sudo rm -rf *.log local-up-cluster.sh.*
 popd
-echo "Cleaning up /ver/run"
+echo "Cleaning up /var/run"
 pushd /var/run
 sudo rm -rf kubernetes
 popd
 
+if [[ -f /var/run/systemd/resolve/resolv.conf ]]; then
+	export KUBELET_RESOLV_CONF=/var/run/systemd/resolve/resolv.conf
+fi
+
 export ALLOW_PRIVILEGED=true
-export KUBELET_RESOLV_CONF=/var/run/systemd/resolve/resolv.conf
 export CGROUP_DRIVER=systemd
 export CONTAINER_RUNTIME_ENDPOINT=/var/run/crio/crio.sock
+export PATH=${HOME}/devel/src/github.com/kubernetes/kubernetes/third_party/etcd:$PATH
 
 echo "Launching local-up-cluster"
 pushd ${HOME}/devel/src/github.com/kubernetes/kubernetes
